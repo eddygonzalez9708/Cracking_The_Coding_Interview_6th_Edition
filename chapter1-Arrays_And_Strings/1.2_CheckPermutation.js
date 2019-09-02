8ad646bc04f2a3d69d132bd61321841abe5de53a
Eddy <edward.gonzalez9708@gmail.com>
2019-09-02 18:39:11 -0400

    Modified description for 1.2 Check Permutation.

--- chapter1-Arrays_And_Strings/1.2_CheckPermutation.js
+++ chapter1-Arrays_And_Strings/1.2_CheckPermutation.js
@@ -1,5 +1,6 @@
 /*
-1.2 Check Permutation: Given two strings, write a method to decide if one is a permutation of the other.
+1.2 Check Permutation: 
+Given two strings, write a method to decide if one is a permutation of the other.
 
 Hints: #7, #84, #722, #737
 */
