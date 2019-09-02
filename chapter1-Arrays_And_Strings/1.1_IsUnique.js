e9d7fbd2e826e8ef8e532efe4bdb9498d26156f0
Eddy <edward.gonzalez9708@gmail.com>
2019-09-02 18:33:04 -0400

    Added a second solution to 1.1 Is Unique.

--- chapter1-Arrays_And_Strings/1.1_IsUnique.js
+++ chapter1-Arrays_And_Strings/1.1_IsUnique.js
@@ -1,25 +1,53 @@
 /* 
-1.1 Is Unique: Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
+1.1 Is Unique: 
+Implement an algorithm to determine if a str has all unique characters. 
+What if you cannot use additional data structures?
 
 Hints: #7, #84, #722, #737
 */
 
-function IsUnique(string) {
+function IsUniqueOne(str) {
+  let mem = {}
 
-    /* 
-    The runtime is O(n^2). There are no additional data structures 
-    used. For each character, check remaining characters for duplicates.
-    */
-  
-    for (let firstChar = 0; firstChar < string.length; firstChar++) {
-      for (let secondChar = firstChar + 1; secondChar < string.length; secondChar++) {
-        if (string[firstChar] === string[secondChar]) return false
-      }
+  /* 
+  The runtime is O(n). The space complexity is O(n). 
+  For each character, check if it exists in the memory cache. 
+  If the character exists in the memory cache, then return false.
+  If the character does not exist is in the memory cache, then add the character 
+  to the memory cache.
+  */
+
+  for (let x = 0; x < str.length; x++) {
+    if (mem[str[x]]) {
+      return false
+    } else {
+      mem[str[x]] = true
     }
-  
-    return true
   }
-  
-console.log(IsUnique('Bed')) // Should return true
-console.log(IsUnique('Eddy')) // Should return false
-console.log(IsUnique('Code')) // Should return true  
+
+  return true
+}
+
+console.log(IsUniqueOne('Bed')) // Should return true
+console.log(IsUniqueOne('Eddy')) // Should return false
+console.log(IsUniqueOne('Code')) // Should return true 
+
+function IsUniqueTwo(str) {
+
+  /* 
+  The runtime is O(n^2). There are no additional data structures 
+  used. For each character, check remaining characters for duplicates.
+  */
+
+  for (let x = 0; x < str.length; x++) {
+    for (let y = x + 1; y < str.length; y++) {
+      if (str[x] === str[y]) return false
+    }
+  }
+
+  return true
+}
+
+console.log(IsUniqueTwo('Bed')) // Should return true
+console.log(IsUniqueTwo('Eddy')) // Should return false
+console.log(IsUniqueTwo('Code')) // Should return true  
