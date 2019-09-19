74a6dd22b9b6c7f1f1d65f3f19280ae6cd257749
Eddy <edward.gonzalez9708@gmail.com>
2019-09-19 13:19:31 -0400

    Added the GitHub Solution for the One Away problem in chapter 1.

--- chapter1-Arrays_And_Strings/1.5_OneAway.js
+++ chapter1-Arrays_And_Strings/1.5_OneAway.js
@@ -13,7 +13,7 @@ pale, bake -> false
 Hints: #23, #97, #130
 */
 
-function oneAway(str1, str2) {
+function oneAwayOne(str1, str2) {
   // Create pointers for both strings
   let ptr1 = 0;
   let ptr2 = 0;
@@ -47,7 +47,78 @@ function oneAway(str1, str2) {
   return edits < 2;
 }
 
-console.log(oneAway('pale', 'pIe')); // Should return true
-console.log(oneAway('pales', 'pale')); // Should return true
-console.log(oneAway('pale', 'bale')); // Should return true 
-console.log(oneAway('pale', 'bake')); // Should return false 
+// Tests
+console.log(oneAwayOne('pale', 'pIe')); // Should return true
+console.log(oneAwayOne('pales', 'pale')); // Should return true
+console.log(oneAwayOne('pale', 'bale')); // Should return true 
+console.log(oneAwayOne('pale', 'bake')); // Should return false 
+
+function oneAwayTwo(string1, string2) {
+  // insert a char for str1 -> remove a char for str2
+  function checkOneMissing(first, second) {
+    if (first.length !== second.length - 1) {
+      return false;
+    } else {
+      var mulligan = false;
+      var fP = 0; // first Pointer
+      var sP = 0; // second Pointer
+      
+      while (fP < first.length) {
+        if (first[fP] !== second[sP]) {
+          if (mulligan) {
+            return false;
+          } else {
+            mulligan = true;
+            sP++; // second length is longer
+          }
+        } else {
+          fP++;
+          sP++;
+        }
+      }
+      
+      return true;
+    }
+  };
+
+  function checkOneDiff(first, second) {
+    if (first.length !== second.length) {
+      return false;
+    } else {
+      var mulligan = false;
+      var fP = 0; // first Pointer
+      var sP = 0; // second Pointer
+      
+      while (fP < first.length) {
+        if (first[fP] !== second[sP]) {
+          if (mulligan) {
+            return false; // more than one mismatch
+          } else {
+            mulligan = true; // use up mulligan
+          }
+        }
+        
+        fP++;
+        sP++;
+      }
+      
+      return true;
+    }
+  };
+  
+  // insert a char for str1 -> remove a char for str2
+  // check one diff
+
+  // console log checks
+  // console.log(string1, string2, 'checkMiss', checkOneMissing(string1, string2));
+  // console.log(string2, string1, 'checkMiss', checkOneMissing(string2, string1));
+  // console.log(string1, string2, 'checkDiff', checkOneDiff(string1, string2));
+
+  return checkOneMissing(string1, string2) || checkOneMissing(string2, string1) || checkOneDiff(string1, string2);
+};
+
+// Test
+console.log(oneAwayTwo('pale', 'ple')); // Should return true
+console.log(oneAwayTwo('pales', 'pale')) // Should return true
+console.log(oneAwayTwo('pale', 'bale')); // Should return true
+console.log(oneAwayTwo('pale', 'bake')); // Should return false
