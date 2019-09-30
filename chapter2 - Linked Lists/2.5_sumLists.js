09e72770d537cd9f76f9aaf75c62c04410501a3c
Eddy <edward.gonzalez9708@gmail.com>
2019-09-29 22:47:40 -0400

    Completed my solution for the Sum Lists Forward problem in chapter 2.

--- chapter2 - Linked Lists/2.5_sumLists.js
+++ chapter2 - Linked Lists/2.5_sumLists.js
@@ -32,6 +32,18 @@ class LinkedList {
     this.tail = null;
   }
 
+  addToHead(val) {
+    const node = new Node(val);
+
+    if (!this.head) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      node.next = this.head;
+      this.head = node;
+    }
+  }
+
   addToTail(val) {
     const node = new Node(val);
 
@@ -54,9 +66,52 @@ class LinkedList {
   }
 };
 
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  addToHead(val) {
+    const node = new Node(val);
+
+    if (!this.head) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.head.prev = node;
+      node.next = this.head;
+      this.head = node;
+    }
+  }
+
+  addToTail(val) {
+    const node = new Node(val);
+
+    if (!this.head) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      node.prev = this.tail;
+      this.tail = node;
+    }
+  }
+
+  printList() {
+    let n = this.head;
+
+    while (n) {
+      console.log(n.val);
+      n = n.next;
+    }
+  }
+};
+
 class Node {
   constructor(val) {
     this.val = val;
+    this.prev = null;
     this.next = null;
   }
 };
@@ -98,48 +153,146 @@ function sumListsReverse(l1, l2) {
   return list;
 }
 
+function sumListsForward(l1, l2) {
+  const doublyList = new DoublyLinkedList();
+  let node1 = l1.tail;
+  let node2 = l2.tail;
+  let carryNum = 0;
+
+  while (node1 || node2 || carryNum) {
+    let total = 0;
+   
+    if (!node1 && !node2) {
+      total = carryNum;
+    } else if (node1 && !node2) {
+      total = node1.val + carryNum;
+      node1 = node1.prev;
+    } else if (!node1 && node2) {
+      total = node2.val + carryNum;
+      node2 = node2.prev;
+    } else {
+      total = node1.val + node2.val + carryNum;
+      node1= node1.prev;
+      node2 = node2.prev;
+    }
+
+    if (total < 10) {
+      doublyList.addToHead(total);
+      carryNum = 0;
+    } else {
+      doublyList.addToHead(total % 10);
+      carryNum = (total - (total % 10)) / 10
+    }
+  }
+
+  return doublyList;
+};
+
 // Tests for My Solution
-console.log("\n*** Sum Lists Reversed ***");
-const list1 = new LinkedList();
-const list2 = new LinkedList();
+// console.log("\n*** Sum Lists Reversed ***");
+// const list1 = new LinkedList();
+// const list2 = new LinkedList();
 
-list1.addToTail(6);
-list1.addToTail(1);
-list1.addToTail(7);
+// list1.addToTail(6);
+// list1.addToTail(1);
+// list1.addToTail(7);
 
-list2.addToTail(2);
-list2.addToTail(9);
-list2.addToTail(5);
+// list2.addToTail(2);
+// list2.addToTail(9);
+// list2.addToTail(5);
 
-const list3 = sumListsReverse(list1, list2);
-list3.printList();
+// const list3 = sumListsReverse(list1, list2);
+// list3.printList();
+// console.log();
+
+// const list4 = new LinkedList();
+// const list5 = new LinkedList();
+
+// list4.addToTail(2);
+// list4.addToTail(4);
+// list4.addToTail(2);
+
+// list5.addToTail(5);
+// list5.addToTail(9);
+
+// const list6 = sumListsReverse(list4, list5);
+// list6.printList();
+// console.log();
+
+// const list7 = new LinkedList();
+// const list8 = new LinkedList();
+
+// list7.addToTail(9);
+// list7.addToTail(7);
+// list7.addToTail(8);
+
+// list8.addToTail(6);
+// list8.addToTail(8);
+// list8.addToTail(5);
+
+// const list9 = sumListsReverse(list7, list8);
+// list9.printList();
+// console.log();
+
+console.log("\n*** Sum Lists Forward ***");
+const doublyList10 = new DoublyLinkedList();
+const doublyList11 = new DoublyLinkedList();
+
+doublyList10.addToTail(6);
+doublyList10.addToTail(1);
+doublyList10.addToTail(7);
+
+doublyList11.addToTail(2);
+doublyList11.addToTail(9);
+doublyList11.addToTail(5);
+
+const doublyList12 = sumListsForward(doublyList10, doublyList11);
+doublyList12.printList();
 console.log();
 
-const list4 = new LinkedList();
-const list5 = new LinkedList();
+const doublyList13 = new DoublyLinkedList();
+const doublyList14 = new DoublyLinkedList();
 
-list4.addToTail(2);
-list4.addToTail(4);
-list4.addToTail(2);
+doublyList13.addToTail(2);
+doublyList13.addToTail(4);
+doublyList13.addToTail(2);
 
-list5.addToTail(5);
-list5.addToTail(9);
+doublyList14.addToTail(5);
+doublyList14.addToTail(9);
 
-const list6 = sumListsReverse(list4, list5);
-list6.printList();
+const doublyList15 = sumListsForward(doublyList13, doublyList14);
+doublyList15.printList();
 console.log();
 
-const list7 = new LinkedList();
-const list8 = new LinkedList();
+const doublyList16 = new DoublyLinkedList();
+const doublyList17 = new DoublyLinkedList();
 
-list7.addToTail(9);
-list7.addToTail(7);
-list7.addToTail(8);
+doublyList16.addToTail(9);
+doublyList16.addToTail(7);
+doublyList16.addToTail(8);
 
-list8.addToTail(6);
-list8.addToTail(8);
-list8.addToTail(5);
+doublyList17.addToTail(6);
+doublyList17.addToTail(8);
+doublyList17.addToTail(5);
 
-const list9 = sumListsReverse(list7, list8);
-list9.printList();
+const doublyList18 = sumListsForward(doublyList16, doublyList17);
+doublyList18.printList();
+console.log();
+
+const doublyList19 = new DoublyLinkedList();
+const doublyList20 = new DoublyLinkedList();
+
+doublyList19.addToTail(9);
+doublyList19.addToTail(9);
+doublyList19.addToTail(9);
+doublyList19.addToTail(9);
+doublyList19.addToTail(9);
+doublyList19.addToTail(9);
+
+doublyList20.addToTail(1);
+doublyList20.addToTail(1);
+doublyList20.addToTail(1);
+
+const doublyList = sumListsForward(doublyList19, doublyList20);
+doublyList.printList();
 console.log();
