77a09a3712f312e2df4a196c701275f65108190c
Eddy <edward.gonzalez9708@gmail.com>
2019-10-01 13:43:55 -0400

    Modified my solution for the Intersection problem in chapter 2.

--- chapter2 - Linked Lists/2.7_intersection/mySolution.js
+++ chapter2 - Linked Lists/2.7_intersection/mySolution.js
@@ -12,28 +12,47 @@ Hints: #20, #45, #55, #65, #76, #93, #111, #120, #129
 const LinkedList = require('../util/LinkedList');
 
 function intersection(node1, node2) {
+  const stack1 = [];
+  const stack2 = [];
+
   while (node1.next || node2.next) {
     if (node1.next && node2.next) {
+      stack1.push(node1);
+      stack2.push(node2);
       node1 = node1.next;
       node2 = node2.next;
     } else if (node1.next && !node2.next) {
+      stack1.push(node1);
       node1 = node1.next; 
     } else {
+      stack2.push(node2);
       node2 = node2.next;
     }
   }
 
-  return node1 === node2;
+  if (node1 === node2) {
+    let intersect = null;
+
+    while (stack1[stack1.length - 1] === stack2[stack2.length - 1]) {
+      intersect = stack1[stack1.length - 1].value;
+      stack1.pop();
+      stack2.pop();
+    }
+
+    return intersect;
+  }
+
+  return null;
 }
 
 let a = new LinkedList('a');
-let b = new LinkedList('a');
-let c = new LinkedList('a');
-let d = new LinkedList('a');
-let e = new LinkedList('a');
-let f = new LinkedList('a');
-let g = new LinkedList('a');
-let h = new LinkedList('a');
+let b = new LinkedList('b');
+let c = new LinkedList('c');
+let d = new LinkedList('d');
+let e = new LinkedList('e');
+let f = new LinkedList('f');
+let g = new LinkedList('g');
+let h = new LinkedList('h');
 
 a.next = b;
 b.next = c;
