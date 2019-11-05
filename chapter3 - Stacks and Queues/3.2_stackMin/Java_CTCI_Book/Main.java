ca0684eda43af4bbad7dda440c520ecd4e99403e
Eddy <edward.gonzalez9708@gmail.com>
2019-11-05 17:14:27 -0500

    Added the second Java CTCI book solution for the Stack Min problem in chapter 3.

--- chapter3 - Stacks and Queues/3.2_stackMin/Java_CTCI_Book/Main.java
+++ chapter3 - Stacks and Queues/3.2_stackMin/Java_CTCI_Book/Main.java
@@ -18,5 +18,25 @@ class Main {
     stack.pop();
 
     System.out.println(stack.min());
+
+    StackWithMin2 stack2 = new StackWithMin2();
+
+    stack2.push(7);
+    stack2.push(7);
+    stack2.push(2);
+    stack2.push(1);
+    stack2.push(0);
+
+    System.out.println(stack2.min());
+
+    stack2.pop();
+    stack2.pop();
+
+    System.out.println(stack2.min());
+
+    stack2.pop();
+    stack2.pop();
+
+    System.out.println(stack2.min());
   }
 }
