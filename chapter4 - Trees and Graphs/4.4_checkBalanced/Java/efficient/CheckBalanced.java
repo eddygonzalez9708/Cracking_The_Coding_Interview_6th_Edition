e88a8d988ded1cee6e5bdeaa166aa59652929bb3
Eddy <edward.gonzalez9708@gmail.com>
2019-12-03 16:12:06 -0500

    Added the Java CTCI Book efficient solution for the Check Balanced problem in chapter 4.

---
+++ chapter4 - Trees and Graphs/4.4_checkBalanced/Java/efficient/CheckBalanced.java
@@ -0,0 +1,25 @@
+package efficient;
+
+public class CheckBalanced {
+  public int checkHeight(TreeNode root) {
+    if (root == null) return -1;
+   
+    int leftHeight = checkHeight(root.left);
+    if (leftHeight == Integer.MIN_VALUE) return Integer.MIN_VALUE; // Pass error up
+
+    int rightHeight = checkHeight(root.left);
+    if (rightHeight == Integer.MIN_VALUE) return Integer.MIN_VALUE; // Pass error up
+
+    int heightDiff = leftHeight - rightHeight;
+
+    if (Math.abs(heightDiff) > 1) {
+      return Integer.MIN_VALUE; // Found error -> pass it back
+    } else {
+      return Math.max(leftHeight, rightHeight) + 1;
+    }
+  }
+
+  public boolean isBalanced(TreeNode root) {
+    return checkHeight(root) != Integer.MIN_VALUE;
+  }
+}
---
+++ chapter4 - Trees and Graphs/4.4_checkBalanced/Java/efficient/TreeNode.java
@@ -0,0 +1,27 @@
+package efficient;
+
+public class TreeNode {
+  int val;
+  TreeNode left;
+  TreeNode right;
+
+  public TreeNode(int val) {
+    this.val = val;
+  }
+
+  public void insert(int val) {
+    if (val < this.val) {
+      if (this.left == null) {
+        this.left = new TreeNode(val);
+      } else {
+        this.left.insert(val);
+      }
+    } else {
+      if (this.right == null) {
+        this.right = new TreeNode(val);
+      } else {
+        this.right.insert(val);
+      }
+    }
+  }
+}
