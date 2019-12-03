ae2073740ef5e9c065d2185e3307fedddc450fb8
Eddy <edward.gonzalez9708@gmail.com>
2019-12-02 19:08:42 -0500

    Added the JavaScript CTCI Book BFS solution for the List of Depths problem in chapter 4.

---
+++ chapter4 - Trees and Graphs/4.3_listOfDepths/Java/MainBFS.java
@@ -0,0 +1,50 @@
+import bfs.TreeNode;
+import bfs.BST;
+import bfs.ListOfDepths;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class MainBFS {
+  public static void printList(ArrayList<LinkedList<TreeNode>> list) {
+    for (LinkedList<TreeNode> ll : list) {
+      for (TreeNode node : ll) {
+        System.out.print(node.val + " ");
+      }
+
+      System.out.println();
+    }
+  }
+
+  public static void main(String args[]) {
+    BST tree = new BST(4);
+
+    tree.insert(2);
+    tree.insert(6);
+    tree.insert(1);
+    tree.insert(3);
+    tree.insert(5);
+    tree.insert(7);
+
+    tree.printLevel();
+
+    System.out.println();
+
+    ListOfDepths listOfDepths = new ListOfDepths();
+    ArrayList<LinkedList<TreeNode>> list1 = listOfDepths.createLevelLinkedList(tree.root);
+
+    printList(list1);
+
+    System.out.println();
+
+    tree.insert(8);
+
+    tree.printLevel();
+
+    System.out.println();
+
+    ArrayList<LinkedList<TreeNode>> list2 = listOfDepths.createLevelLinkedList(tree.root);
+
+    printList(list2);
+  }
+}
